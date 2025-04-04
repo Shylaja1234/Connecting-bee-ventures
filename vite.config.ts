d3c3b772d8841b6513c6e6fc3c4f39f09e9b16c2
Shylaja1234 <shylajagowda34@gmail.com>
2025-04-04 13:15:57 +0530

    Fix build error

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.VITE_BASE_PATH || "/react-vite-deploy",
   server: {
     host: "::",
     port: 8080,
